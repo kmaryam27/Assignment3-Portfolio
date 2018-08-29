0855d081acc6cbea445472f60086b74b167eeae8
k_maryam27 <k_maryam27@yahoo.com>
2018-08-29 18:36:09 -0300

    first commit

---
+++ Portfolio/starter/assets/js/app.js
@@ -0,0 +1,19 @@
+const divClickList1 = document.getElementById('ptf_1');
+divClickList1.style.cursor = 'pointer';
+divClickList1.onclick = function() {
+    location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-1/15-Stu_Practice1/Unsolved/";
+};
+
+const divClickList2 = document.getElementById('ptf_2');
+divClickList2.style.cursor = 'pointer';
+
+divClickList2.onclick = function() {
+    location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-2/16-Stu_Practice2/Unsolved/";
+};
+
+const divClickList3 = document.getElementById('ptf_3');
+divClickList3.style.cursor = 'pointer';
+
+divClickList3.onclick = function() {
+    location.href = "https://kmaryam27.github.io/Assignment2-ResponisiveLayouts-3/17-Stu_Practice3/Unsolved/";
+};
